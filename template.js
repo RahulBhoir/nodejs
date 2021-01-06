489c1ba80e5752c08f9dc4f2862ed972c8fa0f3a
RahulBhoir <rahulpbhoir@outlook.com>
2021-01-06 23:06:48 +0530

    updated the template code

--- template.js
+++ template.js
@@ -1,7 +1,15 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const ejs = require('ejs');
+const mongoose = require('mongoose');
+
 
 const app = express();
+app.use('view engine', 'ejs')
+app.use(express.static('public'));
+mongoose.connect('mongodb://localhost:27107/dbName',{useNewUrlParser:true});
+
+
 
 app.use(bodyParser.urlencoded({extended:true}));
 
